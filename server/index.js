9e4c23f571d0534e65c4bbeabb4fdab39040dbb1
goginim <david.ingook@gmail.com>
2019-07-25 22:38:50 -0400

    added cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const api = require('./src/routes');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
